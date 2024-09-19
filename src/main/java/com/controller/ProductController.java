777ea81da6d53e521b7d56fd572162b709be9028
naitikjpatel <njpatel20031011@gmail.com>
2024-09-19 20:04:08 +0530

    added product get and product list

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,8 +1,14 @@
 package com.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.ProductEntity;
@@ -22,5 +28,39 @@ public class ProductController {
 		System.out.println(productEntity);
 		productRepository.save(productEntity);
 		return productEntity;
+	}
+	
+	//read all products infromation
+	@GetMapping("/getproducts")
+	public List<ProductEntity> getAllProducts(){
+		
+		List<ProductEntity> products=productRepository.findAll();
+		return products;
+		
+	}
+	
+	//get Product by id using pathVariable
+	@GetMapping("/getproducts/{productId}")
+	public ProductEntity getProductById(@PathVariable("productId") Integer productId) {
+	 Optional<ProductEntity> optional=	productRepository.findById(productId);
+	 if(optional.isEmpty()) {
+		 return null;
+	 }else {
+		 ProductEntity productEntity=optional.get();
+		 return productEntity;
+	 }}
+	 
+	 
+
+		//get Product by id using query parameter
+		@GetMapping("/getproduct")
+		public ProductEntity getProductById2(@RequestParam("productId") Integer productId) {
+		 Optional<ProductEntity> optional=	productRepository.findById(productId);
+		 if(optional.isEmpty()) {
+			 return null;
+		 }else {
+			 ProductEntity entity=optional.get();
+			 return entity;
+		 }
 	}
 }
