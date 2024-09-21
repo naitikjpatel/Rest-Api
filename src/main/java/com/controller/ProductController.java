9c393e43c93e9386650d82b1ab7c6c0f3151cf14
naitikjpatel <njpatel20031011@gmail.com>
2024-09-21 15:56:12 +0530

    added delete and update api

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,4 +65,33 @@ public class ProductController {
 			 return entity;
 		 }
 	}
+		
+		
+		
+		// api -> remove product from products table -> delete product
+		// input : productId
+
+		@DeleteMapping("/products/{productId}")
+		public String deleteProductById(@PathVariable("productId") Integer productId) {
+		 Optional<ProductEntity> optional=	productRepository.findById(productId);
+		 if(optional.isEmpty()) {
+			 return "Not Found";
+		 }
+		 else {
+			 productRepository.deleteById(productId);
+			 return "success";
+		 }
+		}
+		
+		
+		// update
+//		input : all product info that we can modify  , productId must be present 
+		
+		@PutMapping("/products")
+		public String updateProduct(@RequestBody ProductEntity productEntity) {
+			//here in save method : if id is not present in the table then it add . if the product id is the present in the table then it update with the new value
+			productRepository.save(productEntity);//id is present
+			
+			return "success";
+		}
 }
